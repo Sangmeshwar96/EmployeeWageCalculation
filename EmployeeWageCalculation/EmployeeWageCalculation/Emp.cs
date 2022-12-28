b4d472d8dcb5c99ed310564ee43123767f39d89b
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-28 20:47:34 +0530

    [Sangmeshwar] Add: Calculating Monthly Wage For Total Working Hours

--- EmployeeWageCalculation/EmployeeWageCalculation/Emp.cs
+++ EmployeeWageCalculation/EmployeeWageCalculation/Emp.cs
@@ -16,10 +16,12 @@ namespace EmployeeWage
             int empWage = 0;
             int wagePerHR = 20;
             int totalWorkingdays = 20;
-            int totalWage = 0;
-            for (int i = 0; i < totalWorkingdays; i++)
+            int totalWorkingHR = 100;
+            int totalEmpHr = 0;
+            int totalDay = 0;
+            do
             {
-     
+        
                 Random random = new Random();
                 int attendCheck = random.Next(0, 3);
                 if (attendCheck == fullTime)
@@ -37,11 +39,14 @@ namespace EmployeeWage
                     empHr = 0;
                     Console.WriteLine("\nThe employee is absent.");
                 }
+                totalEmpHr = totalEmpHr + empHr;
                 empWage = empHr * wagePerHR;
-                Console.WriteLine($"\nEmployee wage is {empWage}");
-                totalWage = totalWage + empWage;
-            }
-            Console.WriteLine($"\nTotal wage for 20 working days is  {totalWage}");
+                Console.WriteLine($"\nEmployee wage is {empWage}.");
+                totalDay++;
+            } while (totalEmpHr <= totalWorkingHR && totalDay < totalWorkingdays);
+            Console.WriteLine($"\nThe total working days per month is {totalWorkingdays} and total working hours is {totalEmpHr}.");
+            empWage = totalEmpHr * wagePerHR;
+            Console.WriteLine($"\nEmployee wage is {empWage}.");
         }
     }
 }
